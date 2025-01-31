ee69443766c7d0afd06916db994793c2c012fbf1
ArthurMukashev <arthur.mukasev@gmail.com>
2025-01-31 18:34:58 +0500

    add prisma and make DB structure

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as process from 'node:process';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 const start = async () => {
     try {
         const PORT = process.env.PORT || 5000;
         const app = await NestFactory.create(AppModule);
+
+        const config = new DocumentBuilder()
+            .setTitle('API')
+            .setDescription('API description')
+            .setVersion('1.0')
+            .addTag('Users')
+            .build();
+        const document = SwaggerModule.createDocument(app, config);
+        SwaggerModule.setup('api', app, document);
 
         await app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
     } catch (e) {
